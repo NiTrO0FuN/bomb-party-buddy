4ea9df432688fe8bb1bba13be99e53769ba2679e
NiTrO0FuN <128875940+NiTrO0FuN@users.noreply.github.com>
2025-09-07 18:56:23 +0200

    Update wordlists

--- words/utils.py
+++ words/utils.py
@@ -47,15 +47,42 @@ def combine_words(source_file, language):
         lf.write("\n".join(combined))
     print(f"Combined words from '{source_file}' into '{lang_file}'.")
 
+def remove_words(source_file, language, separator):
+    lang_file = f"{language}.txt"
+    if not os.path.exists(lang_file):
+        print(f"Language file '{lang_file}' does not exist.")
+        return
+    
+    # Read words from source file
+    with open(source_file, "r", encoding="utf-8") as sf:
+        source_words = sf.read().split(separator)
+        source_words = [unicodedata.normalize("NFKD", w).encode("ascii", "ignore").decode("ascii") for w in source_words]
+
+     # Read words from language file
+    with open(lang_file, "r") as lf:
+        lang_words = lf.read().split("\n")
+
+    filtered = [w for w in lang_words if w not in source_words]
+
+    
+    # Write back to language file
+    with open(lang_file, "w") as lf:
+        lf.write("\n".join(filtered))
+    print(f"Removed words from '{lang_file}' which are present in '{source_file}'.")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Utilities for word files.")
     parser.add_argument("--clean", action="store_true", help="Clean all .txt files in the current directory.")
     parser.add_argument("--combine", nargs=2, metavar=("FILENAME", "LANGUAGE"), help="Combine words from FILENAME into LANGUAGE.txt if it exists.")
+    parser.add_argument("--remove", nargs=2, metavar=("FILENAME", "LANGUAGE"), help="Remove words from LANGUAGE.txt which are present in FILENAME.")
+    parser.add_argument("--separator", "-s", default="\n", help="Separator used to parse FILENAME.")
     args = parser.parse_args()
 
     if args.clean:
         clean_txt_files()
     elif args.combine:
         combine_words(args.combine[0], args.combine[1])
+    elif args.remove:
+        remove_words(args.remove[0], args.remove[1], args.separator)
     else:
         parser.print_usage()
